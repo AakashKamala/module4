e8bddd81cef4d32832e8b4ee75109a5ee5f46474
Aakash <aakashkumar2908@gmail.com>
2025-03-06 23:52:08 +0530

    initials3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express=require("express")
 require("dotenv").config()
+const cors=require("cors")
+const mongoose=require("mongoose")
 
 const app=express()
+app.use(cors())
+app.use(express.json())
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
